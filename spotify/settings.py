af72e7adbaefdea7c1cf27d73d820f58c230ccf4
Antoine Pierlot-Garcin <antoine@bokbox.com>
2013-05-20 19:25:18 +0200

    settings: add missing settings.

--- spotify/settings.py
+++ spotify/settings.py
@@ -2,7 +2,7 @@
 #
 # Settings for session creation
 
-class Settings:
+class Settings(object):
     """
     This class stores settings used when creating a Spotify session.
     """
@@ -12,11 +12,47 @@ class Settings:
     Your application key (binary string).
     """
 
+    ca_certs_filename = None
+    """
+    Path to a file containing the root ca certificates that the peer should be
+    verified with. The file must be a concatenation of all certificates in PEM
+    format. Provided with libspotify is a sample pem file in examples. It is
+    recommended that the application export a similar file from the local
+    certificate store.
+    """
+
     cache_location = 'tmp'
     """
     The location where Spotify will write cache files. This cache include
     tracks, cached browse results and coverarts. Set to ``''`` to disable
     cache.
+    """
+
+    compress_playlists = False
+    """
+    Compress local copy of playlists, reduces disk space usage.
+    """
+
+    device_id = None
+    """
+    Device ID for offline synchronization and logging purposes. The Device Id
+    must be unique to the particular device instance, i.e. no two units must
+    supply the same Device ID. The Device ID must not change between sessions
+    or power cycles. Good examples is the device's MAC address or unique serial
+    number.
+    """
+
+    dont_save_metadata_for_playlists = False
+    """
+    Don't save metadata for local copies of playlists Reduces disk space usage
+    at the expense of needing to request metadata from Spotify backend when
+    loading list.
+    """
+
+    initially_unload_playlists = False
+    """
+    Avoid loading playlists into RAM on startup. See :attr:`Playlist.in_ram`
+    for more details.
     """
 
     proxy = None
@@ -44,8 +80,18 @@ class Settings:
     folder yourself, you can set 'settings_location' to 'cache_location'. 
     """
 
+    tracefile = None
+    """
+    Path to API trace file.
+    """
+
     user_agent = 'pyspotify-example'
     """
     "User-Agent" for your application - max 255 characters long. The User-Agent
     should be a relevant, customer facing identification of your application.
+    """
+
+    userdata = None
+    """
+    User supplied data for your application.
     """
